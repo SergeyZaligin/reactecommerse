c62259f8a9d5b26c339264f58e3cdf07b72a67a9
cobweb <codename.cobweb@gmail.com>
2018-04-15 15:32:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+
+import './store';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Start</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
